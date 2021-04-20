92d72a283e2937826c3eb018753f1b60456e665b
Nina H.Kivanani <34209695+NinaHKivanani@users.noreply.github.com>
2021-04-20 14:53:03 +0200

    ch.1

---
+++ spoken_lg_processing.py
@@ -0,0 +1,13 @@
+
+#--------------------1.spoken_lg_processing------------------------------------#
+
+import wave
+
+# Create audio file wave object
+good_morning = wave.open("good_morning.wav", 'r')
+
+# Read all frames from wave object 
+signal_gm = good_morning.readframes(-1)
+
+# View first 10
+print(signal_gm[:10])
